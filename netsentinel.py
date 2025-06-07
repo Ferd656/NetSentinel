3ed8d6ae9429d488816b766f406479f886472ea3
Ferdinand <ferd656@gmail.com>
2025-06-07 17:12:45 -0600

    GUI tweaks

--- netsentinel.py
+++ netsentinel.py
@@ -1,32 +1,46 @@
+from flask import Flask, render_template
 import nmap
 import socket
 import ipaddress
 
-# Get local IP address
-hostname = socket.gethostname()
-local_ip = socket.gethostbyname(hostname)
+app = Flask(__name__)
 
-# Guess the local network (assume /24 subnet)
-network = ipaddress.ip_network(local_ip + '/24', strict=False)
+def scan_network():
+    hostname = socket.gethostname()
+    local_ip = socket.gethostbyname(hostname)
+    network = ipaddress.ip_network(local_ip + '/24', strict=False)
+    nm = nmap.PortScanner()
+    nm.scan(hosts=network.with_prefixlen, arguments='-O')
+    devices = []
+    for host in nm.all_hosts():
+        ip = host
+        try:
+            name = nm[host].hostname() or socket.gethostbyaddr(host)[0]
+        except Exception:
+            name = "Unknown"
+        os = "Unknown"
+        if 'osmatch' in nm[host] and nm[host]['osmatch']:
+            os = nm[host]['osmatch'][0]['name']
+        devices.append({'ip': ip, 'name': name, 'os': os})
+    return devices
 
-print(f"Scanning network: {network}")
+@app.route('/')
+def index():
+    return render_template('radar.html')
 
-nm = nmap.PortScanner()
+@app.route('/scan')
+def scan():
+    devices = scan_network()
+    return render_template('mainpage.html', devices=devices)
 
-# Scan the network for live hosts and OS detection
-scan_result = nm.scan(hosts=str(network), arguments='-O')
+if __name__ == '__main__':
+    import threading
+    import time
+    import webbrowser
 
-print("\nDevices found:")
-print(f"{'IP Address':<16} {'Hostname':<30} {'OS':<30}")
-print("-"*80)
+    def open_browser():
+        time.sleep(2)
+        webbrowser.open('http://127.0.0.1:5000')
 
-for host in nm.all_hosts():
-    ip = host
-    try:
-        name = nm[host].hostname() or socket.gethostbyaddr(host)[0]
-    except Exception:
-        name = "Unknown"
-    os = "Unknown"
-    if 'osmatch' in nm[host] and nm[host]['osmatch']:
-        os = nm[host]['osmatch'][0]['name']
-    print(f"{ip:<16} {name:<30} {os:<30}")
+    threading.Thread(target=open_browser, daemon=True).start()
+    app.run(debug=False)
